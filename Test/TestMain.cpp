eafbeeff114a6bec4fb4ff62213f4c87d696f1b3
Aldo Nunez <aldonunez1@gmail.com>
2021-06-27 23:53:32 -0700

    Set up memory leak checking in test driver

--- Test/TestMain.cpp
+++ Test/TestMain.cpp
@@ -1,2 +1,16 @@
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+
+
+int main( int argc, char* argv[] )
+{
+    _CrtSetDbgFlag(
+        _crtDbgFlag
+        | _CRTDBG_LEAK_CHECK_DF
+        | _CRTDBG_ALLOC_MEM_DF
+    );
+
+    int result = Catch::Session().run( argc, argv );
+
+    return result;
+}
