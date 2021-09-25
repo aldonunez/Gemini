d6005993eb1ebfd59a635ec062ac8c40b315018c
Aldo Nunez <aldonunez1@gmail.com>
2021-09-24 22:16:24 -0700

    Add tests for more complex scenarios initializing const and var

--- Test/TestAlgolyPtrConst.cpp
+++ Test/TestAlgolyPtrConst.cpp
@@ -520,6 +520,20 @@ TEST_CASE( "Algoly: init const int field from var field", "[algoly][ptr-const][n
     TestCompileAndRunAlgoly( code, CompilerErr::SEMANTICS );
 }
 
+TEST_CASE( "Algoly: init var int field from const field", "[algoly][ptr-const]" )
+{
+    const char code[] =
+        "type R = record a, b end\n"
+        "const R1: R = { a: 1, b: 3 }\n"
+        "var R2: R := { a: 2, b: R1.b }\n"
+        "def a\n"
+        "  R2.a + R2.b\n"
+        "end\n"
+        ;
+
+    TestCompileAndRunAlgoly( code, 5 );
+}
+
 TEST_CASE( "Algoly: init const record by copying", "[algoly][ptr-const]" )
 {
     const char code[] =
@@ -546,6 +560,22 @@ TEST_CASE( "Algoly: init const record by copying var", "[algoly][ptr-const][nega
         ;
 
     TestCompileAndRunAlgoly( code, CompilerErr::SEMANTICS );
+}
+
+TEST_CASE( "Algoly: init var record by copying const", "[algoly][ptr-const]" )
+{
+    const char code[] =
+        "type R = record a, b, c end\n"
+        "const R0 = [10, 20]\n"
+        "const R1: R = { a: 1, b: 2, c: 3 }\n"
+        "var n0 := 30\n"
+        "var R2: R := R1\n"
+        "def a\n"
+        "  R2.a + R2.b + R2.c\n"
+        "end\n"
+        ;
+
+    TestCompileAndRunAlgoly( code, 6 );
 }
 
 TEST_CASE( "Algoly: init const record by record initializer and copying", "[algoly][ptr-const]" )
@@ -576,6 +606,70 @@ TEST_CASE( "Algoly: init const record by record initializer and copying var", "[
         ;
 
     TestCompileAndRunAlgoly( code, CompilerErr::SEMANTICS );
+}
+
+TEST_CASE( "Algoly: init var record by record initializer and copying const", "[algoly][ptr-const]" )
+{
+    const char code[] =
+        "type R = record a, b end\n"
+        "type S = record c: R, r: R end\n"
+        "const R1: R = { a: 3, b: 4 }\n"
+        "var S1: S := { c: { a: 1, b: 2 }, r: R1 }\n"
+        "def a\n"
+        "  S1.c.a + S1.c.b + S1.r.a + S1.r.b\n"
+        "end\n"
+        ;
+
+    TestCompileAndRunAlgoly( code, 10 );
+}
+
+TEST_CASE( "Algoly: init const record field by record initializer and copying const", "[algoly][ptr-const]" )
+{
+    const char code[] =
+        "type R = record a, b end\n"
+        "type S = record c: R, r: R end\n"
+        "const S0: S = { c: { a: 5, b: 6 }, r: { a: 3, b: 4 } }\n"
+        "const S1: S = { c: { a: 1, b: 2 }, r: S0.r }\n"
+        "def a\n"
+        "  S1.c.a + S1.c.b + S1.r.a + S1.r.b\n"
+        "end\n"
+        ;
+
+    TestCompileAndRunAlgoly( code, 10 );
+}
+
+TEST_CASE( "Algoly: init const record field by record initializer and copying var", "[algoly][ptr-const]" )
+{
+    const char code[] =
+        "type R = record a, b end\n"
+        "type S = record c: R, r: R end\n"
+        "var S0: S := { c: { a: 5, b: 6 }, r: { a: 3, b: 4 } }\n"
+        "const S1: S = { c: { a: 1, b: 2 }, r: S0.r }\n"
+        "def a\n"
+        "  S1.c.a + S1.c.b + S1.r.a + S1.r.b\n"
+        "end\n"
+        ;
+
+    TestCompileAndRunAlgoly( code, CompilerErr::SEMANTICS );
+}
+
+TEST_CASE( "Algoly: init var record field by record initializer and copying const", "[algoly][ptr-const]" )
+{
+    const char code[] =
+        "type Q = record a, b end\n"
+        "type R = record a, f: @proc end\n"
+        "type S = record c: Q, r: R end\n"
+        "const C0 = [10, 20]\n"
+        "const S0: S = { c: { a: 5, b: 6 }, r: { a: 3, f: @C } }\n"
+        "var N0 := 30\n"
+        "var S1: S := { c: { a: 1, b: 2 }, r: S0.r }\n"
+        "def a\n"
+        "  S1.c.a + S1.c.b + S1.r.a + (S1.r.f)()\n"
+        "end\n"
+        "def C 4 end\n"
+        ;
+
+    TestCompileAndRunAlgoly( code, 10 );
 }
 
 TEST_CASE( "Algoly: init const int by copying from array element", "[algoly][ptr-const]" )
